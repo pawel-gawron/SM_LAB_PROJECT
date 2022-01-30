8f4b18f095a59f47f93c91eddda2a9be99c20e92
Paweł Gawron <gawron.pawel1999@gmail.com>
2022-01-30 20:22:54 +0100

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,438 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "i2c.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+// LCD
+#include "lcd_i2c.h"
+#include <stdbool.h>
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint16_t readADC;
+float readADC_f;
+const float resolutionADC = 4095.0 / 4.095;
+
+uint16_t user_btn = 0;
+uint16_t duty;
+
+char msg_str[];
+uint16_t msg_len = 20;
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance == USART3)
+	{
+		HAL_UART_Transmit(&huart3, (uint8_t*)msg_str, msg_len, 100 /* ms */);
+		HAL_UART_Receive_IT(&huart3, (uint8_t*)msg_str, msg_len);
+	}
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+
+if(GPIO_Pin == USER_Btn_Pin)
+{
+ user_btn += 1;
+}
+}
+
+float Kp = 1;
+float Ki = 2.78;
+float Kd = 0;
+float dt = 0.1;
+int previous_error = 0;
+int previous_integral = 0;
+int integral;
+int derivative;
+int total_error = 0;
+float p_term, i_term, d_term;
+int PID = 300;
+int u;
+int error;
+int B;
+
+
+
+
+#define TIMCLOCK   72000000
+#define PRESCALAR  72
+
+uint32_t IC_Val1 = 0;
+uint32_t IC_Val2 = 0;
+uint32_t Difference = 0;
+int Is_First_Captured = 0;
+
+
+
+/* Measure Frequency */
+float frequency = 0;
+//uint32_t counter;
+
+// INPUT CAPTURE FUNCTION
+
+
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+	{
+
+
+		if (Is_First_Captured==0) // if the first rising edge is not captured
+		{
+			IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // read the first value
+			Is_First_Captured = 1;  // set the first captured as true
+		}
+
+		else   // If the first rising edge is captured, now we will capture the second edge
+		{
+			IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);  // read second value
+
+
+
+
+			if (IC_Val2 > IC_Val1)
+			{
+				Difference = IC_Val2-IC_Val1;
+			}
+
+			else if (IC_Val1 > IC_Val2)
+			{
+				Difference = (0xffffffff - IC_Val1) + IC_Val2;
+			}
+
+			float refClock = TIMCLOCK/(PRESCALAR);
+
+			frequency = refClock/(Difference*2);
+//			}
+			__HAL_TIM_SET_COUNTER(htim, 0);  // reset the counter
+			Is_First_Captured = 0; // set it back to false
+
+
+		}
+	}
+	  if(PID == 0)
+	  {
+		  frequency = 0;
+	  }
+}
+
+
+// ENCODER
+
+int8_t count_normalized;
+uint16_t counter;
+
+
+void UpdateCounter(void)
+{
+	static uint16_t LastTimerCounter = 0;
+	int TimerDif = htim3.Instance->CNT - LastTimerCounter;
+	if(TimerDif >= 4 || TimerDif <= -4)
+	{
+		TimerDif /= 0.8;
+		count_normalized += (int8_t)TimerDif;
+		if(count_normalized > 100) count_normalized = 100;
+		if(count_normalized < 0) count_normalized = 0;
+		LastTimerCounter = htim3.Instance->CNT;
+		counter = (uint16_t)count_normalized;
+	}
+}
+// UART
+void send_string(char* s)
+{
+	HAL_UART_Transmit(&huart3, (uint8_t*)s, strlen(s), 100);
+
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if(htim->Instance == TIM1)
+  {
+//	 __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, (uint16_t)(readADC_f));
+
+
+  		  u = counter;
+
+
+  		  error = u - frequency;
+
+  		  p_term = (float)(Kp * error);
+
+  		  integral = previous_integral + (error+previous_error);
+  		  previous_integral = integral;
+  		  i_term = Ki*integral*(dt/2);
+
+  		  derivative = (error - previous_error)/dt;
+  		  previous_error = error;
+  		  d_term = Kd*derivative;
+
+  		  PID = (uint16_t)(p_term + i_term + d_term);
+
+  		  if (PID > 1000)
+  		  {
+  			  PID = 1000;
+  		  }
+  		  else if(PID < 0)
+  		  {
+  			  PID =0;
+  		  }
+  		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, PID);
+
+//  		UART
+
+
+  }
+}
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  MX_ADC1_Init();
+  MX_TIM2_Init();
+  MX_I2C1_Init();
+  MX_TIM3_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+
+// Input Capture
+  HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
+
+//  Encoder
+  HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
+
+
+  // LCD init
+  struct lcd_disp disp;
+
+
+  disp.addr = (0x27 << 1);
+  disp.bl = true;
+  lcd_init(&disp);
+
+  sprintf((char *)&disp.f_line, "################");
+  sprintf((char *)&disp.s_line, "################");
+
+  lcd_display(&disp);
+
+
+//  HAL_UART_Receive_IT(&huart3, (uint8_t*)msg_str, msg_len);
+
+//  uint16_t PWM = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+//	  counter = __HAL_TIM_GET_COUNTER(&htim3);
+// odczyt z ADC
+
+	  HAL_ADC_Start(&hadc1);
+		 if (HAL_ADC_PollForConversion(&hadc1, 100) == HAL_OK)
+		 {
+			readADC = HAL_ADC_GetValue(&hadc1);
+			readADC_f = readADC / 4.095;
+		 }
+
+		 sprintf((char *)&disp.f_line, "var: %d", counter);
+		 lcd_display(&disp);
+		 HAL_Delay(500);
+		 UpdateCounter();
+		 HAL_UART_Receive_IT(&huart3, (uint8_t*)msg_str, msg_len);
+
+
+
+
+
+
+	  // zmiana PWM
+
+//	  if( PWM < 1000)
+//	  {
+//	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_3, PWM);
+//		PWM += 100;
+//	  }
+//	  else
+//	  {
+//		  PWM = 0;
+//	  }
+//	  HAL_Delay(3000);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+  /** Configure LSE Drive Capability
+  */
+  HAL_PWR_EnableBkUpAccess();
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3|RCC_PERIPHCLK_I2C1;
+  PeriphClkInitStruct.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+  PeriphClkInitStruct.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
